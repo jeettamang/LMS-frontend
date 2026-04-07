7a1050334811d4138d0a59efc8e186e13b34e8a9
Jeet Tamang <jeettamang011@gmail.com>
2026-04-07 23:54:31 +0545

    render url added

--- src/config/ApiRoute.jsx
+++ src/config/ApiRoute.jsx
@@ -1,2 +1,3 @@
-export const baseURL = "http://localhost:7000/api/v1";
+export const baseURL =
+  "http://localhost:7000/api/v1" || "https://lms-app-lps9.onrender.com/api/v1";
 export const BASE_URL = "http://localhost:7000";
